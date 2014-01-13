b86d464b5b6cdfa1e261cb1c4b8433f23781369a
Christian Papauschek <christian@papauschek.at>
2014-01-13 23:24:13 +0100

    print list of active clusters

--- src/main/scala/clashcode/Main.scala
+++ src/main/scala/clashcode/Main.scala
@@ -10,10 +10,9 @@ object Main extends App {
   override def main(args: Array[String]) {
 
     val port = args(0).toInt
-    val mainConfig = ConfigFactory.parseString("akka.remote.netty.tcp.port = " + port)
-    val config = mainConfig.withFallback(ConfigFactory.load)
-    //println(config.entrySet().toString)
-    //throw new Exception("")
+    val portConfig = ConfigFactory.parseString("akka.remote.netty.tcp.port = " + port)
+    val config = portConfig.withFallback(ConfigFactory.load)
+    val username = config.getString("akka.username")
 
     val system = ActorSystem("cluster", config)
 
@@ -23,11 +22,10 @@ object Main extends App {
         ClusterRouterSettings(totalInstances = 100, routeesPath = "/user/main", allowLocalRoutees = true, useRole = None))),
       name = "router")
 
-    system.actorOf(Props(classOf[MainActor], router, port), "main")
+    system.actorOf(Props(classOf[MainActor], router, username), "main")
 
     readLine()
     system.shutdown()
-    //system2.shutdown()
   }
 
 }
--- src/main/scala/clashcode/MainActor.scala
+++ src/main/scala/clashcode/MainActor.scala
@@ -7,30 +7,37 @@ import java.util.concurrent.TimeUnit
 import scala.concurrent.ExecutionContext.Implicits._
 import akka.util.Timeout
 import akka.actor.ActorIdentity
+import scala.collection.mutable
 
 
 /**
  *
  * */
-class MainActor(router: ActorRef, port: Int) extends Actor {
+class MainActor(router: ActorRef, username: String) extends Actor {
 
   /** timer for interrogation */
   context.system.scheduler.schedule(FiniteDuration(1, TimeUnit.SECONDS), FiniteDuration(1, TimeUnit.SECONDS)) {
     self ! "now"
   }
 
-  implicit val timeout = Timeout(FiniteDuration(1, TimeUnit.SECONDS)) // needed for `?` below
+  //implicit val timeout = Timeout(FiniteDuration(1, TimeUnit.SECONDS)) // needed for `?` below
+
+  val hosts = mutable.Map.empty[String, Long]
 
   def receive = {
-    case "now" =>
 
-      //val identifyRequest = (router ? Identify("x")).mapTo[ActorIdentity]
-      //identifyRequest.map(response => println("Identity: " + response))
+    case "now" =>
+      val minTime = System.currentTimeMillis() - 3000;
+      println(hosts.filter(_._2 >= minTime).keySet)
       router ! "ping"
 
-    case "ping" => sender ! ("pong " + port)
-    case "pong" => println("pong")
-    case x => println(x)
+    case "ping" =>
+      sender ! ("pong", username)
+
+    case ("pong", name: String) =>
+      val key = if (name.length == 0) Seq(sender.path.address.host, sender.path.address.port).flatten.mkString(":") else name
+      hosts.update(key, System.currentTimeMillis())
+
   }
 
 }
