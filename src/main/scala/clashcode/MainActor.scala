954856c7ca0f4926a68b9a3ef544a8d19c6da8be
Christian Papauschek <christian@papauschek.at>
2014-01-22 12:39:47 +0100

    implement basic actor

--- src/main/scala/clashcode/MainActor.scala
+++ src/main/scala/clashcode/MainActor.scala
@@ -1,42 +1,23 @@
 package clashcode
 
 import akka.actor._
-import akka.pattern.{ ask, pipe }
 import scala.concurrent.duration.FiniteDuration
 import java.util.concurrent.TimeUnit
 import scala.concurrent.ExecutionContext.Implicits._
-import akka.util.Timeout
-import akka.actor.ActorIdentity
 import scala.collection.mutable
 
 
-/**
- *
- * */
 class MainActor(router: ActorRef, username: String) extends Actor {
 
-  /** timer for interrogation */
-  context.system.scheduler.schedule(FiniteDuration(1, TimeUnit.SECONDS), FiniteDuration(1, TimeUnit.SECONDS)) {
-    self ! "now"
+  /** timed actions */
+  context.system.scheduler.schedule(FiniteDuration(10, TimeUnit.SECONDS), FiniteDuration(10, TimeUnit.SECONDS)) {
+    self ! "every-10-seconds"
   }
-
-  //implicit val timeout = Timeout(FiniteDuration(1, TimeUnit.SECONDS)) // needed for `?` below
-
-  val hosts = mutable.Map.empty[String, Long]
 
   def receive = {
 
-    case "now" =>
-      val minTime = System.currentTimeMillis() - 3000;
-      println(hosts.filter(_._2 >= minTime).keySet)
-      router ! "ping"
-
-    case "ping" =>
-      sender ! ("pong", username)
-
-    case ("pong", name: String) =>
-      val key = if (name.length == 0) Seq(sender.path.address.host, sender.path.address.port).flatten.mkString(":") else name
-      hosts.update(key, System.currentTimeMillis())
+    case "every-10-seconds" =>
+      router ! "hello"
 
   }
 
