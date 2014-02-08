6674086de9010c8b42dffc7ccfd6d467a875b12a
wwan <wwagner4@gmail.com>
2014-02-08 08:17:24 +0100

    next direction works fine

--- src/test/scala/clashcode/SceneCreatorSuite.scala
+++ src/test/scala/clashcode/SceneCreatorSuite.scala
@@ -42,21 +42,90 @@ class SceneCreatorSuite extends FunSuite {
   }
 
   val dummyCans = Set.empty[Pos]
+  val fieldSize = 10;
 
   ignore("step [0 0] [0 1] robot E") {
     val step = FieldStep(FieldPos(0, 0), FieldPos(0, 1))
     val robot = RobotView(Pos(1, 1), N)
-    val stages: List[Stage] = stepToStages(step, robot)
+    val stages: List[Stage] = stepToStages(step, robot, fieldSize)
     val expectedStages = List(
       Stage(RobotView(Pos(1, 1), E), dummyCans),
       Stage(RobotView(Pos(1, 1), SE), dummyCans),
       Stage(RobotView(Pos(1, 1), S), dummyCans),
       Stage(RobotView(Pos(1, 2), S), dummyCans),
       Stage(RobotView(Pos(1, 3), S), dummyCans))
-      assert(stages === expectedStages)
+    assert(stages === expectedStages)
   }
 
-  def stepToStages(step: FieldStep, robot: RobotView): List[Stage] = ???
+  {
+    case class NextDirResult(fromx: Int, fromy: Int, tox: Int, toy: Int, expectedDir: Direction)
+    val validSteps = List(
+      NextDirResult(0, 0, 0, 1, S),
+      NextDirResult(0, 0, 1, 0, E),
+      NextDirResult(1, 1, 0, 1, W),
+      NextDirResult(1, 1, 1, 0, N),
+      NextDirResult(1, 1, 1, 2, S),
+      NextDirResult(2, 1, 1, 1, W))
+
+    for (ndr <- validSteps) {
+      test(s"next direction valid step $ndr") {
+        val s = FieldStep(FieldPos(ndr.fromx, ndr.fromy), FieldPos(ndr.tox, ndr.toy))
+        val nd = nextDirection(s, fieldSize)
+        assert(nd === ndr.expectedDir)
+      }
+    }
+  }
+  {
+    case class NextDirResult(fromx: Int, fromy: Int, tox: Int, toy: Int)
+    val invalidSteps = List(
+      NextDirResult(0, 0, 0, 0),
+      NextDirResult(0, 0, 1, 1),
+      NextDirResult(1, 1, 1, 3),
+      NextDirResult(0, 1, -1, 1),
+      NextDirResult(1, 1, 4, 2),
+      NextDirResult(2, 2, 2, 4))
+
+    for (ndr <- invalidSteps) {
+      test(s"next direction valid step $ndr") {
+        val s = FieldStep(FieldPos(ndr.fromx, ndr.fromy), FieldPos(ndr.tox, ndr.toy))
+        intercept[IllegalArgumentException] {
+          nextDirection(s, fieldSize)
+        }
+      }
+    }
+  }
+
+  def nextDirection(step: FieldStep, fieldSize: Int): Direction = {
+    def assertInBounds(value: Int): Unit = {
+      if (value < 0 || value >= fieldSize) throw new IllegalArgumentException(s"Value $value is out of bound for field size $fieldSize. $step")
+    }
+    assertInBounds(step.from.x)
+    assertInBounds(step.from.y)
+    assertInBounds(step.to.x)
+    assertInBounds(step.to.y)
+    if (step.from.x == step.to.x) {
+      if (step.from.y > step.to.y) {
+        if (step.from.y - step.to.y > 1) throw new IllegalArgumentException(s"The step in y direction is greater than one. $step")
+        else N
+      } else if (step.from.y < step.to.y) {
+        if (step.to.y - step.from.y > 1) throw new IllegalArgumentException(s"The step in y direction is greater than one. $step")
+        else S
+      } else throw new IllegalArgumentException(s"No movement. $step")
+    } else if (step.from.y == step.to.y) {
+      if (step.from.x > step.to.x) {
+        if (step.from.x - step.to.x > 1) throw new IllegalArgumentException(s"The step in x direction is greater than one. $step")
+        else W
+      } else if (step.from.x < step.to.x) {
+        if (step.to.x - step.from.x > 1) throw new IllegalArgumentException(s"The step in x direction is greater than one. $step")
+        else E
+      } else throw new IllegalArgumentException(s"No movement. $step")
+    } else throw new IllegalArgumentException(s"Moved along two axes. $step")
+  }
+
+  def stepToStages(step: FieldStep, robot: RobotView, fieldSize: Int): List[Stage] = {
+    val dir: Direction = nextDirection(step, fieldSize)
+    ???
+  }
 }
 
 case class FieldStep(from: FieldPos, to: FieldPos)
