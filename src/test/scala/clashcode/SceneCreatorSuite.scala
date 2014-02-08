96de8ade3aa53f049041df809a88b1c52faee4a7
wwan <wwagner4@gmail.com>
2014-02-08 13:51:29 +0100

    step to stages works

--- src/test/scala/clashcode/SceneCreatorSuite.scala
+++ src/test/scala/clashcode/SceneCreatorSuite.scala
@@ -44,7 +44,7 @@ class SceneCreatorSuite extends FunSuite {
   val dummyCans = Set.empty[Pos]
   val fieldSize = 10;
 
-  ignore("step [0 0] [0 1] robot E") {
+  test("step [0 0] [0 1] robot E") {
     val step = FieldStep(FieldPos(0, 0), FieldPos(0, 1))
     val robot = RobotView(Pos(1, 1), N)
     val stages: List[Stage] = stepToStages(step, robot, fieldSize)
@@ -94,6 +94,37 @@ class SceneCreatorSuite extends FunSuite {
       }
     }
   }
+  {
+    case class DiffResult(from: Direction, to: Direction, expected: Int)
+    val situations = List(
+      DiffResult(N, NE, 1),
+      DiffResult(N, E, 2),
+      DiffResult(N, S, 4),
+      DiffResult(N, SW, -3),
+      DiffResult(E, E, 0),
+      DiffResult(E, W, 4),
+      DiffResult(E, S, 2),
+      DiffResult(E, N, 2))
+    for (s <- situations) {
+      test(s"Direction diff $s") {
+        val d = DirectionUtil.diff(s.from, s.to)
+        assert(d === s.expected)
+      }
+    }
+  }
+  {
+    case class TurnListResult(startDir: Direction, times: Int, expected: List[Direction])
+    val situations = List(
+      TurnListResult(N, 1, List(NE)),
+      TurnListResult(N, 2, List(NE, E)),
+      TurnListResult(N, -1, List(NW)))
+    for (s <- situations) {
+      test(s"Turn list $s") {
+        val l = DirectionUtil.turnList(s.startDir, s.times)
+        assert(l === s.expected)
+      }
+    }
+  }
 
   def nextDirection(step: FieldStep, fieldSize: Int): Direction = {
     def assertInBounds(value: Int): Unit = {
@@ -123,12 +154,86 @@ class SceneCreatorSuite extends FunSuite {
   }
 
   def stepToStages(step: FieldStep, robot: RobotView, fieldSize: Int): List[Stage] = {
-    val dir: Direction = nextDirection(step, fieldSize)
-    ???
+    def turn(nextDir: Direction): List[Stage] = {
+      val prevDir = robot.dir
+      val diff = DirectionUtil.diff(prevDir, nextDir)
+      DirectionUtil.turnList(robot.dir, diff).map(d => Stage(RobotView(robot.pos, d), dummyCans))
+    }
+    def move(nextDir: Direction): List[Stage] = nextDir match {
+      case N => List(
+        Stage(RobotView(Pos(robot.pos.x, robot.pos.y - 1), nextDir), dummyCans),
+        Stage(RobotView(Pos(robot.pos.x, robot.pos.y - 2), nextDir), dummyCans))
+      case E => List(
+        Stage(RobotView(Pos(robot.pos.x + 1, robot.pos.y), nextDir), dummyCans),
+        Stage(RobotView(Pos(robot.pos.x + 2, robot.pos.y), nextDir), dummyCans))
+      case S => List(
+        Stage(RobotView(Pos(robot.pos.x, robot.pos.y + 1), nextDir), dummyCans),
+        Stage(RobotView(Pos(robot.pos.x, robot.pos.y + 2), nextDir), dummyCans))
+      case W => List(
+        Stage(RobotView(Pos(robot.pos.x - 1, robot.pos.y), nextDir), dummyCans),
+        Stage(RobotView(Pos(robot.pos.x - 1, robot.pos.y), nextDir), dummyCans))
+      case _ => throw new IllegalArgumentException(s"Robot can only move N, E, S or W. Not $nextDir")
+    }
+    if (step.from.x == step.to.x && step.from.y == step.to.y) {
+      val nextRobot = RobotView(robot.pos, DirectionUtil.turnRight(robot.dir))
+      // TODO The cans must come somehow out of the step to be able to check if cans where collected
+      List(Stage(robot, dummyCans))
+    } else {
+      val ndir: Direction = nextDirection(step, fieldSize)
+      turn(ndir) ::: move(ndir)
+    }
   }
 }
 
 case class FieldStep(from: FieldPos, to: FieldPos)
+
+case object DirectionUtil {
+  def turnRight(actualDir: Direction): Direction = actualDir match {
+    case N => NE
+    case NE => E
+    case E => SE
+    case SE => S
+    case S => SW
+    case SW => W
+    case W => NW
+    case NW => N
+  }
+  def turnLeft(actualDir: Direction): Direction = actualDir match {
+    case N => NW
+    case NW => W
+    case W => SW
+    case SW => S
+    case S => SE
+    case SE => E
+    case E => NE
+    case NE => N
+  }
+  def diff(from: Direction, to: Direction): Int = {
+    def toNum(dir: Direction): Int = dir match {
+      case N => 0
+      case NE => 1
+      case E => 2
+      case SE => 3
+      case S => 4
+      case SW => 5
+      case W => 6
+      case NW => 7
+    }
+    val d = math.abs(toNum(from) - toNum(to))
+    if (d > 4) d - 8 else d
+  }
+
+  def turnList(startDirection: Direction, times: Int): List[Direction] = {
+    if (times == 0) Nil
+    else if (times > 0) {
+      val nextDir = turnRight(startDirection)
+      nextDir :: turnList(nextDir, times - 1)
+    } else {
+      val nextDir = turnLeft(startDirection)
+      nextDir :: turnList(nextDir, times + 1)
+    }
+  }
+}
 
 case object PathUtil {
 
