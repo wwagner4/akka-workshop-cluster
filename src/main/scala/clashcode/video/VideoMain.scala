2ff945226e7b7dd88b0b2bf3da140ae33f921c4a
wwan <wwagner4@gmail.com>
2014-02-09 08:20:55 +0100

    added swing device factory

--- src/main/scala/clashcode/video/VideoMain.scala
+++ src/main/scala/clashcode/video/VideoMain.scala
@@ -6,21 +6,27 @@ import clashcode.video.swing._
 object VideoMain extends App {
 
   val fieldSize = 10
+  val framesPerSecond = 20
 
   val strCode = "03530311022335213110315511111120251141140200400110522540004423424544141444444444142541204404414145445445424454151340002434334143"
   //val strCode = "51301322323332310310015011211525211111100030050100035520005502144444444444444444114441114434454345441441440045000444300140034051"
   //val strCode = "51301351323352311515012111511323201151120550440004411221333414434444304444444444411451544454454344443442040305454535030515343550"
   val stages = SceneCreator.stringCodeToStages(strCode, fieldSize, 24L)
+  val device = SwingDeviceFactory.device
+  Video.play(device, stages, fieldSize, 20)
 
-  val device = new SwingDevice(createGraphics)
+}
 
-  def createGraphics(g: Graphics2D): AwtGraphics = {
+object SwingDeviceFactory {
+  
+  def device: Device = _device
+  
+  private lazy val _device: SwingDevice = new SwingDevice(createGraphics)
+
+  private def createGraphics(g: Graphics2D): AwtGraphics = {
     new AwtRectGraphicsImages(0.6, 0.05) {
       def graphics: Graphics2D = g
-      def drawArea: DrawArea = device.drawArea
+      def drawArea: DrawArea = _device.drawArea
     }
   }
-
-  Video.play(device, stages, 10, 12)
-
 }
