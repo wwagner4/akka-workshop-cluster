6ce6d17b3b287895d5053be5f28e88b36385a276
wwan <wwagner4@gmail.com>
2014-02-11 14:05:22 +0100

    multivideo working

--- src/main/scala/clashcode/video/VideoMain.scala
+++ src/main/scala/clashcode/video/VideoMain.scala
@@ -6,15 +6,21 @@ import scala.concurrent.duration._
 
 object VideoMain extends App {
 
-  val video = Video("A really bad robot\nShowing only the first steps, the rest is boring\n\nfitness = -10",
+  val v1 = Video("A really bad robot\nShowing only the first steps, the rest is boring\n\nfitness = -10",
     3.second,
     "03530311022335213110315511111120251141140200400110522540004423424544141444444444142541204404414145445445424454151340002434334143",
-    Some(50),
+    Some(30),
+    238476L)
+
+  val v2 = Video("Another robot\n\nfitness = 97840",
+    2.second,
+    "32211522203053512111122122322133011214240300302120305251330342334444432414443414434442434444444422144242403414212434224453421212",
+    None,
     238476L)
 
   val framesPerSecond = 15
-
-  val stages = VideoCreator.create(video: Video, framesPerSecond)
+  
+  val stages = List(v1, v2).flatMap(v => VideoCreator.create(v, framesPerSecond))
   val device: Device = SwingDeviceFactory(framesPerSecond).device
   device.playOnes(stages)
 
