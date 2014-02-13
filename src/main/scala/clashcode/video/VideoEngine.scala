77dad2a48e21480764c52eb9c1712dea443dcdd2
wwan <wwagner4@gmail.com>
2014-02-13 19:30:48 +0100

    added videoEngine. can read file

---
+++ src/main/scala/clashcode/video/VideoEngine.scala
@@ -0,0 +1,24 @@
+package clashcode.video
+
+case class ResultEntry(fitness: Int, name: String, code: String)
+
+object VideoEngine extends App {
+
+  println("Reading File")
+  val results: List[ResultEntry] = readFile("workshop-erge.txt").sortBy(_.fitness).reverse
+  println(results.mkString("\n"))
+
+  private def readFile(filename: String): List[ResultEntry] = {
+    import scala.io.Source
+    import java.io.File
+    val linesList = Source.fromFile(new File(filename)).getLines().toSet
+    val set = linesList.map(parseLine(_))
+    set.toList
+  }
+
+  private def parseLine(line: String): ResultEntry = {
+    val lineItems = line.split("\\s+").filter(_ != "")
+    assert(lineItems.size == 3, s"Size of ${lineItems.toList.mkString("##")} must be 3")
+    ResultEntry(lineItems(0).trim.toInt, lineItems(1).trim, lineItems(2).trim)
+  }
+}
